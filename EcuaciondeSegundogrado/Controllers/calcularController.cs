7c5be80049c4a4e61b8241fd7e2894d6a5b301eb
Frederick <frederickperezdelacruz@outlook.com>
2020-02-02 18:55:14 -0400

    Ecuacion de segundo grado

---
+++ EcuaciondeSegundogrado/Controllers/calcularController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcuaciondeSegundogrado.Controllers
+{
+    public class calcularController : Controller
+    {
+        public IActionResult Index(double Numero1, double Numero2, double Numero3)
+        {
+            double resultado1, resultado2, M;
+            M = Math.Pow(Numero1, Numero2);
+            resultado1 = (-Numero2 + (Math.Sqrt(M - 4 * Numero1 * Numero3))) / ( 2 * Numero1);
+            resultado2 = (-Numero2 - (Math.Sqrt(M - 4 * Numero1 * Numero3))) /(2 * Numero1);
+
+            ViewBag.resulfinal1 = resultado1;
+            ViewBag.resulfinal2 = resultado2;
+            return View();
+        }
+       
+    }
+}
